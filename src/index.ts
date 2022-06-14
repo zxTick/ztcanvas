ef2153611043221de559367ad87e8c2566f5059a
alexzhang1030 <1642114555@qq.com>
2022-06-14 22:52:53 +0800

    feat: line init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './canvasEngine'
 export * from './rect'
 export * from './act'
+export * from './line'
--- src/rect.ts
+++ src/rect.ts
@@ -1,44 +1,44 @@
-import { FillOptions } from "./canvasEngine";
-import { CanvasEngine } from "./canvasEngine";
-import { Noop } from "./types/event";
-import { RectShape, ShapeType } from "./types/shape";
+import type { CanvasEngine, RenderOptions } from './canvasEngine'
+import type { Noop } from './types/event'
+import type { RectShape } from './types/shape'
+import { ShapeType } from './types/shape'
 
 export interface RectOptions {
-  x: number;
-  y: number;
-  shape: "";
-  w: number;
-  h: number;
-  zIndex: number;
+  x: number
+  y: number
+  shape: ''
+  w: number
+  h: number
+  zIndex: number
 }
 
 export class Rect {
-  path2D: Path2D = new Path2D();
-  id: symbol = Symbol();
-  figureInformation!: RectShape;
-  noop: Noop = {};
-  zIndex: number = -1;
+  path2D: Path2D = new Path2D()
+  id = Symbol('Rect')
+  figureInformation!: RectShape
+  noop: Noop = {}
+  zIndex = -1
   constructor(options: RectOptions) {
-    this.machiningGraphics(options);
-    this.injectFigureInformation(options);
-    this.zIndex = options.zIndex || -1;
+    this.machiningGraphics(options)
+    this.injectFigureInformation(options)
+    this.zIndex = options.zIndex || -1
   }
 
   private machiningGraphics(options: RectOptions) {
-    const { x, y, w, h } = options;
-    this.path2D.rect(x, y, w, h);
+    const { x, y, w, h } = options
+    this.path2D.rect(x, y, w, h)
   }
 
-  public fill(canvasEngine: CanvasEngine, options: FillOptions) {
-    const { color } = options;
-    canvasEngine.ctx.fillStyle = color || "";
-    canvasEngine.ctx.fill(this.path2D);
+  public render(canvasEngine: CanvasEngine, options: RenderOptions) {
+    const { color } = options
+    canvasEngine.ctx.fillStyle = color || ''
+    canvasEngine.ctx.fill(this.path2D)
   }
 
   private injectFigureInformation(options: RectOptions) {
     this.figureInformation = {
       ...options,
       shape: ShapeType.Rect,
-    };
+    }
   }
 }
